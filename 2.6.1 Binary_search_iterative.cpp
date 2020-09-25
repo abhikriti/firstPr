88c0cfd7dacae0c76dba82951199ead775e6cfad
abhikriti <71879280+abhikriti@users.noreply.github.com>
2020-09-25 16:09:14 +0530

    Add files via upload

---
+++ 2.6.1 Binary_search_iterative.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> //works like magic in c++
+#include<algorithm>
+using namespace std;
+
+int bin_search(int arr[], int n, int key)
+{
+	int l,h,mid;
+	l=0;
+	h=n-1;
+	while(l<=h)
+	{
+		mid = (l+h)/2;
+		if(arr[mid] == key)
+		return mid;
+		
+		else if(arr[mid] > key)
+		{
+			h = mid -1;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[]={10,20,30,40,50};
+	int size = sizeof(arr)/sizeof(arr[0]);
+     
+    int result = bin_search(arr,size,40);
+	(result == -1) ? printf("Element is present") : printf("Elemet is present at %d",result); 
+	return 0;
+}
