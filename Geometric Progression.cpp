f779f10b9ffc7fc2d348071b6f415e20ce13dd7a
abhikriti <71879280+abhikriti@users.noreply.github.com>
2021-10-02 12:40:20 +0530

    Added GP code

---
+++ Geometric Progression.cpp
@@ -0,0 +1,21 @@
+// radhe radhe
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define inf 1e18
+
+
+int main() {
+
+	FIO;
+#ifndef ONLINE_JUDGE
+	freopen("/home/viper/Desktop/CB/Searching/input17f.in", "r", stdin);
+	freopen("/home/viper/Desktop/CB/Searching/output17f.in", "w", stdout);
+#endif
+
+	cout << "helloe" << endl;
+
+	return 0;
+}
+
